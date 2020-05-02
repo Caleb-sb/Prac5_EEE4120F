491f0ce63c476321972ab84cff5f62575d7cf26d
taboka <nydtab001@myuct.ac.za>
2020-05-02 21:29:30 +0200

    new commit1

--- quartsine.v
+++ quartsine.v
@@ -21,19 +21,50 @@
 module quartsine(
     input clka,
     input [7:0] addra,
-    output [10:0] douta);
+    output reg [10:0] douta2);
 
 reg ena = 1;
 reg wea = 0;
 reg [10:0] dina = 0;
+reg [5:0] val;
+wire [10:0] douta;
+
+always @* begin
+    case({addra[7],addra[6]})
+        2'b00: begin 
+            val = addra[5:0];
+            douta2 = douta;
+        end
+        2'b01: begin
+            val = ~addra[5:0];
+            douta2 = douta;
+        end
+        2'b10: begin
+            val = addra[5:0];
+            douta2 = 1024-(douta-1024);
+        end
+        2'b11: begin
+            val = ~addra[5:0];
+            douta2 = 1024-(douta-1024);
+        end
+    endcase
+end
 
 sinequarter_blk_mem_gen_0 dut(
-        .clka(clka),
-        .ena(ena),
-        .wea(wea),      
-        .addra(addra),  
-        .dina(dina),    
-        .douta(douta)
+        clka,
+        ena,
+        wea,      
+        val,  
+        dina,    
+        douta
         );
+
+//always @* begin
+//   casez({addra[7],addra[6]})
+//        2'b00: douta2 <= douta;
+//        2'b10: douta2 <= douta;
+//        2'b11: douta2 <= douta;
+//   endcase
+//end
 
 endmodule
